b3d8971fdcc8c746e7bdbf431441ab8de4f34dee
Minghang Yang <ytlw@outlook.com>
2025-09-19 01:23:15 +0800

    重命名文件

--- include/listnode/listnode.c
+++ include/listnode/listnode.c
@@ -52,55 +52,16 @@ struct ListNode* generateListNode(stream* ps) {
     return ans;
 }
 
-struct ListNode** newListNodeArray(char ch[]) {
-    int n = strlen(ch);
-    struct ListNode** ans = NULL;
-    int pos = 0;
-    int i = 0;
-    int ansIdx = 0;
-    int capacity = 4;
-    while (ch[i] != ']' && ch[i] != '\0') {
-        if (ch[i] == ',' || ch[i] == '[') {
-            ++i;
-            continue;
-        }
-
-        if (ch[i] == '{') {
-            // 暂不可用
-            struct ListNode* head = newListNode(0, NULL);
-            i = pos;
-            if (ans == NULL) {
-                ans = malloc(sizeof(struct ListNode*) * capacity);
-                ans[ansIdx] = head;
-                ++ansIdx;
-            } else {
-                if (ansIdx < capacity) {
-                    ans[ansIdx] = head;
-                    ++ansIdx;
-                } else {
-                    if (capacity < 1024) {
-                        capacity <<= 1;
-                    } else {
-                        capacity = capacity + (capacity >> 1);
-                    }
-
-                    struct ListNode** ans2 = malloc(sizeof(struct ListNode*) * capacity);
-                    ans2[ansIdx] = head;
-                    ++ansIdx;
-
-                    for (int j = ansIdx - 2; j >= 0; --j) {
-                        ans2[j] = ans[j];
-                    }
-                    struct ListNode** tmp = ans;
-                    ans = ans2;
-                    for (int j = ansIdx - 2; j >= 0; --j) {
-                        free(tmp[j]);
-                    }
-                    free(tmp);
-                }
-            }
-        }
-
+struct ListNode** generateListNodeArray(stream* ps) {
+    struct ListNode* ans = NULL;
+    int status = read(ps);
+    if (status == END) {
+        return NULL;
     }
+
+    do {
+
+    } while (status == NOT_FULL_LINE);
+
     return ans;
 }
